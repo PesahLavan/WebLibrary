fd53d975f0928a56f884e8babaeb6ca1daab56a0
Beljaev_ Pavel <beljaev.pavel.kr@gmail.com>
2016-10-18 17:38:43 +0200

    add Facelets

---
+++ src/main/java/com/pesahlavan/weblibrary/dao/impl/GenreDAOImpl.java
@@ -0,0 +1,28 @@
+package com.pesahlavan.weblibrary.dao.impl;
+
+import com.pesahlavan.weblibrary.dao.interfaces.GenreDAO;
+import com.pesahlavan.weblibrary.entities.Genre;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class GenreDAOImpl implements GenreDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Genre> getGenres() {
+        return sessionFactory.getCurrentSession().createCriteria(Genre.class).list();
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/pesahlavan/weblibrary/dao/interfaces/GenreDAO.java
@@ -0,0 +1,12 @@
+package com.pesahlavan.weblibrary.dao.interfaces;
+
+
+import com.pesahlavan.weblibrary.entities.Genre;
+
+import java.util.List;
+
+public interface GenreDAO {
+
+    List<Genre> getGenres();
+
+}
