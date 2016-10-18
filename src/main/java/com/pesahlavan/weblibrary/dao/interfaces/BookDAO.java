89d568f0cf9c51b8997dca07617e86ec99b637e5
Beljaev_ Pavel <beljaev.pavel.kr@gmail.com>
2016-10-18 16:59:21 +0200

    add Sampling Terms

--- src/main/java/com/pesahlavan/weblibrary/dao/interfaces/BookDAO.java
+++ src/main/java/com/pesahlavan/weblibrary/dao/interfaces/BookDAO.java
@@ -8,7 +8,7 @@ import com.pesahlavan.weblibrary.entities.Genre;
 import java.util.List;
 
 
-public interface LibraryDAO {
+public interface BookDAO {
 
     List<Book> getBooks();
     List<Book> getBooks(Author author);
